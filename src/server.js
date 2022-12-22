f5fda26b7c6c720de24f75521d23c8d6e98daa02
Mateus Macêdo <mateusamacedo5@gmail.com>
2022-12-21 23:11:03 -0300

    feat: ranking route

--- src/server.js
+++ src/server.js
@@ -3,12 +3,14 @@ import cors from "cors"
 import dotenv from "dotenv"
 import usersRouters from "./routes/usersRoutes.js"
 import urlsRouters from "./routes/urlsRoutes.js"
+import rankingRouters from "./routes/rankingRoutes.js"
 
 const app = express();
 app.use(express.json())
 app.use(cors())
 app.use(usersRouters)
 app.use(urlsRouters)
+app.use(rankingRouters)
 
 const port = process.env.PORT || 4000
 
